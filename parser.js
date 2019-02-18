1337712e7dd4375b6c36f305890c13b18b60b7ff
Andrew Au Yeung <andrewcauyeung@gmail.com>
2019-02-18 13:24:39 -0500

    2nd document parse works

--- parser.js
+++ parser.js
@@ -1,37 +1,47 @@
 $(document).ready(function() {
   var fileResults = [];
   function submitFile() {
-    var file = document.getElementById("input").files;
-    onChange(file);
-    for (var i = 0; i < file; i++) {}
+    document.getElementById("input").files;
+    for (var i = 0; i < document.getElementById("input").files.length; i++) {
+      onChange(
+        document.getElementById("input").files[i],
+        document.getElementById("input").files[i]["name"]
+      );
+    }
+    crossReference(fileResults[0], fileResults[1]);
   }
 
-  function onChange(event) {
+  function onChange(event, filename) {
     var reader = new FileReader();
-    reader.onload = onReaderLoad;
+    reader.onload = function(event) {
+      console.log(event.target.result);
+      var temp = $.csv.toArrays(event.target.result);
+      console.log(temp);
+      if (filename.includes("grades")) {
+        fileResults.push(parseGradeCSV(temp));
+      } else if (filename.includes("info")) {
+        fileResults.push(parseCSV(temp));
+      }
+      console.log(fileResults);
+    };
     reader.readAsText(event);
   }
 
-  function onReaderLoad(event) {
-    console.log(event.target.result);
-    var temp = $.csv.toArrays(event.target.result);
-    console.log(temp);
-    fileResults.push(parseCSV(temp));
-  }
-
-  $(".add-title-btn").click(function() {
-    $(".key-container").append(
-      '<div class="input-field col s6">' +
-        '<input placeholder="Title 1" id="title-1" type="text" />' +
-        "</div>"
-    );
-  });
-
-  // document.getElementById("input").addEventListener("change", onChange);
-  // document.getElementById("input2").addEventListener("change", onChange);
-  // document.getElementById("input3").addEventListener("change", onChange);
   document.getElementById("file-submit").addEventListener("click", submitFile);
 });
+
+function crossReference(result1, result2) {
+  console.log(
+    result1.map((a, i) => Object.assign({}, a, { myNewAttribute: result2[i] }))
+  );
+  // for (var i = 0; i < result1.length; i++) {
+  //   for (var x = 0; x < result2.length; x++) {
+  //     if (result1[i]["SBID"]  == result2[x]["SBID"]) {
+
+  //     }
+  //   }
+  // }
+}
 
 function parseCSV(splitArray) {
   var currentStudentInfo = setupDictionary(splitArray);
@@ -46,7 +56,7 @@ function parseCSV(splitArray) {
         if (!isNaN(splitArray[row][col])) {
           //RESET: Current Student Info
           console.log(currentStudentInfo);
-          var temp = setupDictionary(splitArray);
+          var temp = setupDictionary(splitArray, 0);
           studentDictArr.push(currentStudentInfo);
           console.log(studentDictArr);
           currentStudentInfo = temp;
@@ -64,12 +74,71 @@ function parseCSV(splitArray) {
   return studentDictArr;
 }
 
-function setupDictionary(splitArray) {
+// function setupDictionary(splitArray) {
+//   var currentStudentInfo = {};
+//   for (var col = 0; col < splitArray[0].length; col++) {
+//     if (currentStudentInfo[splitArray[0][col]] != "") {
+//       currentStudentInfo[splitArray[0][col]] = "";
+//     }
+//   }
+//   return currentStudentInfo;
+// }
+
+function setupDictionary(splitArray, indexStart, indexEnd) {
   var currentStudentInfo = {};
-  for (var col = 0; col < splitArray[0].length; col++) {
+  if (indexEnd == null) {
+    indexEnd = splitArray[0].length;
+  }
+  for (var col = indexStart; col < indexEnd; col++) {
     if (currentStudentInfo[splitArray[0][col]] != "") {
       currentStudentInfo[splitArray[0][col]] = "";
     }
   }
   return currentStudentInfo;
+}
+
+function parseGradeCSV(splitArray) {
+  var currentStudentInfo = {};
+  var currentCourseInfo = {};
+  var studentDictArr = [];
+
+  for (var row = 1; row < splitArray.length; row++) {
+    for (var col = 0; col < splitArray[row].length; col++) {
+      if (splitArray[row][col] != "") {
+        //CASE: FIRST COLUMN OF ROW
+        if (col == 0) {
+          console.log(currentStudentInfo);
+          var temp = setupDictionary(splitArray, 0, 5);
+          temp["Courses"] = [];
+          studentDictArr.push(currentStudentInfo);
+          console.log(studentDictArr);
+          currentStudentInfo = temp;
+          currentStudentInfo[splitArray[0][col]] = splitArray[row][col];
+        }
+        //CASE: New Course info
+        else if (col == 5) {
+          console.log(currentCourseInfo);
+          var temp = setupDictionary(splitArray, 5);
+          currentStudentInfo["Courses"].push(currentCourseInfo);
+          currentCourseInfo = temp;
+          var tempValue = currentCourseInfo[splitArray[0][col]];
+          var newValue = tempValue.concat(splitArray[row][col]);
+          currentCourseInfo[splitArray[0][col]] = newValue;
+        }
+        //CASE: Course info Key/value
+        else if (col > 5) {
+          var tempValue = currentCourseInfo[splitArray[0][col]];
+          var newValue = tempValue.concat(splitArray[row][col]);
+          currentCourseInfo[splitArray[0][col]] = newValue;
+        }
+        //CASE: ALL OTHER KEY/VALUES
+        else {
+          var tempValue = currentStudentInfo[splitArray[0][col]];
+          var newValue = tempValue.concat(splitArray[row][col]);
+          currentStudentInfo[splitArray[0][col]] = newValue;
+        }
+      }
+    }
+  }
+  return studentDictArr;
 }
